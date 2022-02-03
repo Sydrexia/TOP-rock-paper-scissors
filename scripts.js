bc04b7d349035c9907b73fffa54065d027ff5891
Andrew Glach <andrew.glach@gmail.com>
2022-02-02 17:50:50 -0700

    Score wouldn't save because form refreshed page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 function returnChoice(){
     let options = ['rock', 'paper', 'scissors'];
     let choice = options[(Math.floor(Math.random() * 3) + 1)];
@@ -17,24 +20,39 @@ function returnChoice(){
         if (input == 'rock' && choice == 'rock') {
             alert("It's a tie! You both chose rock.")
         } else if (input == 'rock' && choice == 'paper') {
-            alert("Looks like you lose! Rock loses to paper!")
-        } else { alert("You win! Rock beats scissors!")}
+            alert("Looks like you lose! Rock loses to paper!");
+            computerScore++;
+        } else { alert("You win! Rock beats scissors!");
+            playerScore++;    
+        }
+        document.getElementById('p-score').innerHTML = playerScore;
+        document.getElementById('c-score').innerHTML = computerScore;    
     }
     
     function compareChoiceIfPaper(){
         if (input == 'paper' && choice == 'paper') {
             alert("It's a tie! You both chose paper.")
         } else if (input == 'paper' && choice == 'scissors') {
-            alert("Looks like you lose! Paper loses to scissors!")
-        } else { alert("You win! Paper beats rock!")}
+            alert("Looks like you lose! Paper loses to scissors!");
+            computerScore++;
+        } else { alert("You win! Paper beats rock!");
+            playerScore++;
+        }
+        document.getElementById('p-score').innerHTML = playerScore;
+        document.getElementById('c-score').innerHTML = computerScore;
     }
     
     function compareChoiceIfScissors(){
         if (input == 'scissors' && choice == 'scissors') {
             alert("It's a tie! You both chose scissors.")
         } else if (input == 'scissors' && choice == 'rock') {
-            alert("Looks like you lose! Scissors loses to rock!")
-        } else { alert("You win! Scissors beats paper!")}
+            alert("Looks like you lose! Scissors loses to rock!");
+            computerScore++;
+        } else { alert("You win! Scissors beats paper!");
+            playerScore++;
+        }
+        document.getElementById('p-score').innerHTML = playerScore;
+        document.getElementById('c-score').innerHTML = computerScore;
     }
 }
 
